53880718f913d21097b6067fbb5a5cd92e6e713d
teddy1565 <teddy1565@gmail.com>
2020-11-24 12:28:01 +0800

    MD5 HASH

--- src/nodePDF.js
+++ src/nodePDF.js
@@ -84,24 +84,71 @@ let PrivateTools = {
         for(let i=0;i<(str.length/512);i++){
             BigGroup[i]="";
         }
-        for(let i=0;i<(str.length/512);i++){
+        for(let i=0;i<=(str.length/512);i++){
             while(str.length!=0){
                 BigGroup[i]+=str[0];
                 str = str.slice(1);
+                if(str.length%512==0)break;
             }
         }
         //SmallGroup will grouping 16 groups with 512bit string,every SmallGroup will be 32bit
         let SmallGroup = [];
+        //Initial array
         for(let i=0;i<(BigGroup.length)*16;i++){
-            
+            SmallGroup[i]="";
+        }
+        //Grouping
+        for(let i=0;i<(BigGroup.length)*16;i++){
+            for(let j=0;j<32;j++){
+              SmallGroup[i] += BigGroup[parseInt(i/16)].slice(0,1);
+              BigGroup[parseInt(i/16)] = BigGroup[parseInt(i/16)].slice(1);
+            }
         }
         //Setup shift amounts and initial variable
         const {R1,R2,R3,R4}={R1:[7,12,17,22],R2:[5,9,14,20],R3:[4,11,16,23],R4:[6,10,15,21]};
         const {A,B,C,D}={A:"67452301",B:"efcdab89",C:"98badcfe",D:"10325476"};
+        const {F,G,H,I,FF,GG,HH,II}={
+            F:(x,y,z)=>{
+                return (x&y)|((~x)&z);
+            },
+            G:(x,y,z)=>{
+                return (x&z)|(y&(~z));
+            },
+            H:(x,y,z)=>{
+                return x^y^z;
+            },
+            I:(x,y,z)=>{
+                return y^(x|(~z));
+            },
+            FF:(a,b,c,d,x,s,ac)=>{
+                a += (F(b, c, d)&0xFFFFFFFF) + x + ac;
+                a = ((a&0xFFFFFFFF)<< s) | ((a&0xFFFFFFFF) >>> (32 - s));
+                a += b;
+                return (a&0xFFFFFFFF);
+            },
+            GG:(a,b,c,d,x,s,ac)=>{
+                a += (G(b, c, d)&0xFFFFFFFF) + x + ac;
+                a = ((a&0xFFFFFFFF) << s) | ((a&0xFFFFFFFF) >>> (32 - s));
+                a += b;
+                return (a&0xFFFFFFFF);
+            },
+            HH:(a,b,c,d,x,s,ac)=>{
+                a += (H(b, c, d)&0xFFFFFFFF) + x + ac;
+                a = ((a&0xFFFFFFFF) << s) | ((a&0xFFFFFFFF) >>> (32 - s));
+                a += b;
+                return (a&0xFFFFFFFF);
+            },
+            II:(a,b,c,d,x,s,ac)=>{
+                a += (I(b, c, d)&0xFFFFFFFF) + x + ac;
+                a = ((a&0xFFFFFFFF) << s) | ((a&0xFFFFFFFF) >>> (32 - s));
+                a += b;
+                return (a&0xFFFFFFFF);
+            },
+        };
         return str;
     },
 };
-PrivateTools.MD5("AAAAA");
+PrivateTools.MD5("A");
 let nodePDF={
     loadPDFpath:loadPDFpath,
     splitPDF:splitPDF,
