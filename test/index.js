d22df686e91c8a1d77903d9ceb33f95325dced69
teddy1565 <teddy1565@gmail.com>
2020-11-26 10:57:30 +0800

    test

--- test/index.js
+++ test/index.js
@@ -4,3 +4,4 @@ let fs = require('fs');
 let data = fs.readFileSync(PDFpath);
 data = NP.splitPDF(data);
 fs.writeFileSync("/Users/zhenkaixiong/temp/out.json", JSON.stringify(data));
+console.log(1);
